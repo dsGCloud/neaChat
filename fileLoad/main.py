523e48fd1af0539a760018d842c07628db60248e
Daniel Sanchez <nea@Daniels-MacBook-Air.local>
2023-10-12 21:15:09 -0600

    GCF format

--- fileLoad/main.py
+++ fileLoad/main.py
@@ -1,19 +1,42 @@
+from langchain.document_loaders import GCSDirectoryLoader
 from langchain.embeddings import VertexAIEmbeddings
 from langchain.document_loaders import GCSFileLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import MatchingEngine
-
-
 from cloudevents.http import CloudEvent
+from google.cloud import aiplatform
+
 
 import functions_framework
 
+#function to load documents from GCS bucket
+def load_docs(projectID, bucketName):
+    loader = GCSDirectoryLoader(project_name=projectID, bucket=bucketName)
+    documents = loader.load()
+    return documents
+
+#function to split documents into chunk size
+def split_docs(documents,chunk_size=500,chunk_overlap=20):
+  text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+  docs = text_splitter.split_documents(documents)
+  return docs
 
 # Triggered by a change in a storage bucket
 @functions_framework.cloud_event
 def file_load(cloud_event: CloudEvent) -> tuple:
 
     data = cloud_event.data
+
+    aiplatform.init(
+    project='nom-llm-001',
+
+    # the Vertex AI region you will use
+    location='us-central1',
+
+    # Google Cloud Storage bucket in same region as location
+    # used to stage artifacts
+    staging_bucket='gs://neachat-artifacts',
+    )
 
     # Define Text Embeddings model
     embedding = VertexAIEmbeddings()
@@ -29,14 +52,10 @@ def file_load(cloud_event: CloudEvent) -> tuple:
     )
 
     # Define Cloud Storage file loader to read a document
-    loader = GCSFileLoader(project_name=me.project_id,
-        bucket=url.split("/")[2],
-        blob='/'.join(url.split("/")[3:])
-    )
+    documents = load_docs(me.project_id, me.gcs_bucket_name)
 
     # Split document into chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    doc_splits = text_splitter.split_documents(data["name"])
+    doc_splits = split_docs(documents)
 
     # Add embeddings of document chunks to Matching Engine
     texts = [doc.page_content for doc in doc_splits]
